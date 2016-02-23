8e4c78fce7c1507acd0bd4a9be65989ce0c3d712
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2016-02-23 17:38:36 -0600

    Reset
    RESET

--- CharacterPlans.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public interface CharacterPlans
-{
-   public void move(String direction);
-   public void lookAround();
-   public void grabItem();
-   public void talk();
-}
