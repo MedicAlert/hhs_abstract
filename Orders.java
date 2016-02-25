0e33b643451e027967c27d5c9a99400a0c50a11e
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2016-02-24 20:03:40 -0600

    Order/Runner completion
    completed that part ayy ylmao

--- Orders.java
+++ Orders.java
@@ -1,15 +1,88 @@
 interface OrderPlan
 {
-   void getOrders();
-   
    String getType();
    String getETA();
    String getName();
-   String getCompletion();
+   String getAddress();
+   boolean getCompletion();
    
    void setType(String input);
-   void getETA(String input);
-   void getName(String input);
+   void setETA(String input);
+   void setName(String input);
+   void setAddress(String input);
+   void markComplete(boolean complete);
    
    String toString();   
+}
+
+abstract class CommonOrders implements OrderPlan
+{
+   public String orderType; //priority or standard
+   public String ETA; //DD/MM/YY
+   public String name; //name of recipent
+   public String address; //address of recipent
+   public boolean isComplete; //has reached
+   
+   public String getType(){return orderType;}
+   
+   public String getETA(){return ETA;}
+   
+   public String getName(){return name;}
+   
+   public String getAddress(){return address;}
+   
+   public boolean getCompletion(){return isComplete;}
+   
+   public void setType(String input){ orderType = input; }
+   public void setETA(String input){ ETA = input; }
+   public void setName(String input){ name = input; }
+   public void setAddress(String input){ address = input; }
+   public void markComplete(boolean complete){ isComplete = complete; }
+}
+
+class Packages extends CommonOrders
+{
+   private String subject;
+   
+   public Packages(String subject, String type, String ETA, String name, String address, boolean complete)
+   {
+      this.subject = subject;
+      orderType = type;
+      this.ETA = ETA;
+      this.name = name;
+      this.address = address;
+      isComplete = complete;
+   }
+   
+   public String getSubject(){return subject;}
+   public void setSubject(String input) {subject = input;}
+   public String toString()
+   {
+      String returner = getName() + "'s package details:\n Package Name: " + getSubject() + "\n Order Type: " + getType() + "\n ETA: " + getETA() + "\n Address: " + getAddress() + "\n Has Reached: " + getCompletion();
+      return returner;
+   }
+}
+
+class Letters extends CommonOrders
+{
+   private boolean isPersonal = false;
+
+   public Letters(boolean personal, String type, String ETA, String name, String address, boolean complete)
+   {
+      isPersonal = personal;
+      orderType = type;
+      this.ETA = ETA;
+      this.name = name;
+      this.address = address;
+      isComplete = complete;
+   }
+   
+   public boolean getPersonal(){return isPersonal;}
+   public void setPersonal(boolean personal) {isPersonal = personal;}
+   
+   public String toString()
+   {
+      String returner = getName() + "'s package details:\n Is it a personal letter: " + getPersonal() + "\n Order Type: " + getType() + "\n ETA: " + getETA() + "\n Address: " + getAddress() + "\n Has Reached: " + getCompletion();
+      return returner;
+   }
 }
--- Runner.java
+++ Runner.java
@@ -26,14 +26,30 @@ public class Runner
    {
       System.out.println("Status of Scratch-A-Bear Shipping Co.");
       System.out.println("-----------------------------------");
-      System.out.println();
-      
+      System.out.println();      
       System.out.println();
  
- 
+      System.out.println("Order Statuses:");
+      System.out.println("-----------------------------------");
       
+      ArrayList<CommonOrders> masterlist = new ArrayList<CommonOrders>();
+      masterlist.add(new Packages("Teddy Bears", "priority", "28/2/2016", "Vijay Rajagopal", "1234 Fake Ln., Tennessee, U.S.", false));
+      masterlist.add(new Packages("Nuclear Weapons", "standard", "28/3/2026", "Not-American_Government", "1232 Fake Ln., FakeState, SomeAsianCountry", false));
+      masterlist.add(new Packages("Scuba Gear", "priority", "12/6/2016", "Alejandro Scroggs", "1234 Fake Ln., Mississippi, U.S.", true));
+      masterlist.add(new Packages("Bear Whistle", "super-priority", "2/25/2016", "Lukas Marinovic", "1234 Fake Ln., Georgia, U.S.", true));
+      masterlist.add(new Letters(true, "super-priority", "12/12/2016", "Lukas Marinovic and Vijay Rajagopal", "1234 Fake Ln., Georgia, U.S.", true));
+      
+      System.out.println("All active orders:");
+      System.out.println();
+      for(CommonOrders obj: masterlist)
+      {
+         System.out.println(obj);
+         System.out.println();
+      }
+
       System.out.println("Transportation Status:");
       System.out.println("-----------------------------------");
+      
       Truck truck = new Truck();
       Plane plane = new Plane();
       Ship ship = new Ship();
